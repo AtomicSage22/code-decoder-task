360129cac479e25805d4a5aaa4ac0e817a618db4
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-09-19 22:47:57 +0530

    add bar graphs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { PieChart, Pie } from 'recharts'
+import { PieChart, Pie, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Line, LineChart } from 'recharts'
 
 const p1data01 = [
   {
@@ -54,6 +54,109 @@ const p1data02 = [
   }
 ];
 
+const barData = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300
+  }
+]
+
+const data = [
+  {
+    "day": "05-01",
+    "temperature": [
+      -1,
+      10
+    ]
+  },
+  {
+    "day": "05-02",
+    "temperature": [
+      2,
+      15
+    ]
+  },
+  {
+    "day": "05-03",
+    "temperature": [
+      3,
+      12
+    ]
+  },
+  {
+    "day": "05-04",
+    "temperature": [
+      4,
+      12
+    ]
+  },
+  {
+    "day": "05-05",
+    "temperature": [
+      12,
+      16
+    ]
+  },
+  {
+    "day": "05-06",
+    "temperature": [
+      5,
+      16
+    ]
+  },
+  {
+    "day": "05-07",
+    "temperature": [
+      3,
+      12
+    ]
+  },
+  {
+    "day": "05-08",
+    "temperature": [
+      0,
+      8
+    ]
+  },
+  {
+    "day": "05-09",
+    "temperature": [
+      -3,
+      5
+    ]
+  }
+]
 
 
 export default function Home() {
@@ -68,9 +171,10 @@ export default function Home() {
             <h2 className='text-4xl text-blue-600 font-semibold'>$6,621,280</h2>
           </div>
           <div className='bg-white'>
-          <PieChart width={230} height={250}>
-            <Pie data={p1data01} dataKey="value" nameKey="name" cx="65%" cy="60%" outerRadius={50} fill="#8884d8" />
-            <Pie data={p1data02} dataKey="value" nameKey="name" cx="65%" cy="60%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+          <PieChart width={290} height={250}>
+            <Tooltip />
+            <Pie data={p1data01} dataKey="value" nameKey="name" cx="50%" cy="60%" outerRadius={50} fill="#72A956" />
+            <Pie data={p1data02} dataKey="value" nameKey="name" cx="50%" cy="60%" innerRadius={60} outerRadius={80} fill="#8D56A9" label />
           </PieChart>
           </div>
         </div>
@@ -80,6 +184,11 @@ export default function Home() {
             <h2 className='text-4xl text-red-600 font-semibold'>$1,630,270</h2>
           </div>
           <div className='bg-white'>
+          <PieChart width={290} height={250}>
+            <Tooltip />
+            <Pie data={p1data01} dataKey="value" nameKey="name" cx="50%" cy="60%" outerRadius={50} fill="#A6A059" />
+            <Pie data={p1data02} dataKey="value" nameKey="name" cx="50%" cy="60%" innerRadius={60} outerRadius={80} fill="#595FA6" label />
+          </PieChart>
           </div>
         </div>
         <div className='grid grid-rows-[1fr_2fr] gap-4'>
@@ -88,6 +197,11 @@ export default function Home() {
             <h2 className='text-5xl text-blue-900 font-semibold'>75.38 %</h2>
           </div>
           <div className='bg-white'>
+          <PieChart width={290} height={250}>
+            <Tooltip />
+            <Pie data={p1data01} dataKey="value" nameKey="name" cx="50%" cy="60%" outerRadius={50} fill="#BE5D41" />
+            <Pie data={p1data02} dataKey="value" nameKey="name" cx="50%" cy="60%" innerRadius={60} outerRadius={80} fill="#41A2BE" label />
+          </PieChart>
           </div>
         </div>
       </section>
@@ -98,17 +212,44 @@ export default function Home() {
             <h2 className='text-5xl text-blue-900 font-semibold'>1.10 %</h2>
           </div>
           <div className='bg-white'>
-
+            <PieChart width={290} height={250}>
+            <Tooltip />
+            <Pie data={p1data01} dataKey="value" nameKey="name" cx="50%" cy="60%" outerRadius={50} fill="#B946A4" />
+            <Pie data={p1data02} dataKey="value" nameKey="name" cx="50%" cy="60%" innerRadius={60} outerRadius={80} fill="#46B95B" label />
+          </PieChart>
           </div>
         </div>
-        <div className='bg-white'>
+        <div className='bg-white flex items-end pb-2 pr-4'>
+        <BarChart width={590} height={430} data={barData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#724CB3" />
+          <Bar dataKey="uv" fill="#8DB34C" />
+        </BarChart>
         </div>
       </section>
-      <section className='bg-white'>
-        
+      <section className='bg-white flex items-end justify-center'>
+      <LineChart width={920} height={430} data={barData}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
       </section>
-      <section className='bg-white'>
-        
+      <section className='bg-white flex justify-center items-end'>
+      <BarChart width={920} height={430} data={data} margin={{top: 20, right: 20, bottom: 20, left: 20}} >
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="temperature" fill="#8884d8" />
+      </BarChart>
       </section>
     </main>
   )
